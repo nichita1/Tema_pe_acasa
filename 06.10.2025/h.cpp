6e20df1fa49155c8b69a11119320b8ee58c2ae33
nichita1 <129595015+nichita1@users.noreply.github.com>
2025-11-11 20:32:31 +0200

    Add files via upload

--- 06.10.2025/h.cpp
+++ 06.10.2025/h.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
+#include <stdlib.h> 
+#include <time.h>   
 using namespace std;
-
-int main(){
-    const int M=100;
-    int n; 
-    int mat[M][M];
-    cin>>n; 
-    for(int i=0;i<n;++i)
-    {
-        for(int j=0;j<n;++j)
-        {
-
-            cin>>mat[i][j];
+int N_MAX = 10; 
+int main() {
+    srand(time(0)); 
+    int N = 5; 
+    int lst = 10;
+    int ldr = 50;
+    int A[N_MAX][N_MAX];
+    long long sumaElementeImpare = 0;
+    int numarElementeImpare = 0;
+    if (N > N_MAX) {
+        return 1;
+    }
+    if (lst > ldr) {
+        int temp = lst;
+        lst = ldr;
+        ldr = temp;
+    }
+    int interval_marime = ldr - lst + 1;
+    if (interval_marime <= 0) interval_marime = 1;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            A[i][j] = lst + rand() % interval_marime;
         }
     }
-    int suma=0;
-    int contor=0;
-    for(int i=0;i<n;++i)
-    {
-        int j=n-1-i;
-        if(mat[i][j]%2!=0){
-            suma+=mat[i][j];
-            contor++;
+    cout << "Matrice:\n";
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cout << A[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < N; ++i) {
+        int j = N - 1 - i; 
+        int element = A[i][j];
+        if (element % 2 != 0) {
+            sumaElementeImpare += element;
+            numarElementeImpare++;
         }
     }
-    if(contor>0){
-        int media=suma/contor;
-        cout<<media<<endl;
-    }else{
-        cout<<"Nu exista elemente impare pe diagonala secundara."<<endl;
+    cout << "\nRezultat:\n";
+    if (numarElementeImpare == 0) {
+        cout << "Media este 0.\n";
+    } else {
+        double medie = sumaElementeImpare * 1.0 / numarElementeImpare;
+        
+        cout << "Suma impare: " << sumaElementeImpare << "\n";
+        cout << "Nr. impare: " << numarElementeImpare << "\n";
+        cout << "Media: " << medie << "\n";
     }
     return 0;
 }
