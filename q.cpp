d202231d5a42881c0bca8f71f1353bbb9e63fabe
nichita1 <129595015+nichita1@users.noreply.github.com>
2025-11-17 14:16:33 +0200

    Add files via upload

---
+++ q.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    struct Punct {
+    double x;
+    double y;
+};
+Punct P[30];
+int i , j , d , n;
+cout<<"nr de puncte:"<<endl;
+cin>>n;
+    for(int i1=0;i1<n;i1++){
+cout<<"punct["<<i1<<"]"<<endl;
+cout<<"coord x:";
+cin>>P[i1].x;
+cout<<"coord y:";
+cin>>P[i1].y;
+    }
+return 0;
+}
