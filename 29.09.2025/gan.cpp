a2130d19de0998db0a482fd5cbea899e6e952a58
nichita1 <129595015+nichita1@users.noreply.github.com>
2025-11-30 19:15:07 +0200

    Update gan.cpp

--- 29.09.2025/gan.cpp
+++ 29.09.2025/gan.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
+
+const int M = 200;
+
+bool contine_cifra_5(int x) {
+    if (x < 0) {
+        x = -x;
+    }
+    if (x == 0) {
+        return false;
+    }
+    while (x > 0) {
+        if (x % 10 == 5) {
+            return true;
+        }
+        x /= 10;
+    }
+    return false;
+}
 
 int p_c(int x){
     if(x<0){
@@ -15,71 +32,56 @@ int p_c(int x){
     return x;
 }
 
-bool id_c(int x){
-    if(x<0){
-        x=-x;
-    }
-    if(x<10){
-        return true;
-    }
-    int u=x%10;
-    x/=10;
-    while(x>0){
-        if(x%10!=u){
-            return false;
-        }
-        x/=10;
-    }
-    return true;
-}
-
 int main(){
-    const int M=200;
-    int n,a[M];
+    int n, a[M];
     
-    cin>>n; 
-    for(int i=0;i<n;++i){
-        cin>>a[i];
+    cin >> n;
+    if (n > M || n < 0) n = M; 
+
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
     }
+    
     int C;
-    cin>>C;
-    int B;
-    cin>>B;
-    int j=0;
-    for(int i=0;i<n;++i){
-        if(p_c(a[i])!=C){
-            a[j]=a[i];
+    cin >> C;
+
+    int index_primul_5 = -1;
+    for (int i = 0; i < n; ++i) {
+        if (contine_cifra_5(a[i])) {
+            index_primul_5 = i;
+            break;
+        }
+    }
+
+    if (index_primul_5 != -1) {
+        for (int i = index_primul_5; i < n - 1; ++i) {
+            a[i] = a[i + 1];
+        }
+        n--;
+        
+        cout << "Dupa Cerinta 1:" << endl;
+        for(int i = 0; i < n; ++i){
+            cout << a[i] << " ";
+        }
+    } else {
+        cout << "Dupa Cerinta 1: Tabloul ramane neschimbat." << endl;
+    }
+    cout << endl;
+    
+    int j = 0;
+    for(int i = 0; i < n; ++i){
+        if(p_c(a[i]) != C){
+            a[j] = a[i];
             j++;
         }
     }
-    n=j;
-  
-    cout<<"Dupa eliminare (P2):"<<endl;
-    for(int i=0;i<n;++i){
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
-    int i=0;
-    while(i<n){
-        if(id_c(a[i])){
-            for(j=n;j>i+1;--j){
-                a[j]=a[j-1];
-            }
-            
-            a[i+1]=B;
-            n++;
-            i+=2;
-        }else{
-            i++;
-        }
-    }
-    cout<<"Dupa inserare (P3):"<<endl;
-    for(int k=0;k<n;++k){
-        cout<<a[k]<<" ";
-    }
-    
-  }
-    cout<<endl;
-    return 0;
+    n = j;
 
+    cout << "Dupa Cerinta 2:" << endl;
+    for(int i = 0; i < n; ++i){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
